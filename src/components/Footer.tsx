bfe72738ea4e206311e7de54a67e959271c0fb5d
arsci <russerya@gmail.com>
2023-08-06 09:57:25 -0700

    add analytics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import * as Nav from './Navigation'
+import * as Socials from './Socials'
   
 export default function Footer() {
   return (
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
-          {Nav.navigation.main.map((item) => (
+          {Nav.navigation.map((item) => (
             <div key={item.name} className="pb-6">
               <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
                 {item.name}
@@ -14,7 +15,7 @@ export default function Footer() {
           ))}
         </nav>
         <div className="mt-10 flex justify-center space-x-10">
-          {Nav.navigation.social.map((item) => (
+          {Socials.socials.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
