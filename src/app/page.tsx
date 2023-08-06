32d1b4faecd5eae226664531f8f06bd75dbba6da
arsci <russerya@gmail.com>
2023-08-06 10:09:10 -0700

    fix analytics, client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-"use client"
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Header />
       <Footer />
-      <Analytics />
     </main>
   )
 }
