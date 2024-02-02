8d77eda4b0c8c20726aa01427405469973ca2bd7
Ryan Russell <russerya@gmail.com>
2024-02-01 21:36:36 -1000

    fix navbar collapse (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <div className="py-24 sm:py-24">
       <div className="mx-auto max-w-5xl px-6 lg:px-8">
-        <h2 className="text-3xl font-bold tracking-tight blog-index sm:text-4xl">Latest Blog Posts</h2>
+        <h2 className="text-3xl font-bold tracking-tight blog-index sm:text-4xl max-w-">Latest Blog Posts</h2>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t lines pt-4 sm:mt-8 sm:pt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             <article key={latestTechPost._id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
