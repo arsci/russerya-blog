cff67aa2421ca745dd147e3926adc80676e36386
arsci <russerya@gmail.com>
2024-03-08 22:56:26 -0800

    fmt

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -102,7 +102,7 @@ export function NewsletterFormStacked() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Oops! Either you're already subscribed,<br />
+                Oops! Either you&apos;re already subscribed,<br />
                 or there was another failure. You may try again.
               </button>
             </>
